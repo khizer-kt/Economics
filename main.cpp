88c09b38ccca2ab7a90e73e03deb26deec8fb78e
Khizer Tariq <99535489+khizer-kt@users.noreply.github.com>
2022-12-29 20:24:49 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-#pragma once
 #include <iostream>
-#include "LinkedList.h"
-#include "parser.h"
+#include "AVL.h"
 using namespace std;
-
 int main() {
-    // Create an empty linked list
-    LinkedList list;
-
-    // Read the data from a file and store it in the list
-    parse_all(list);
-
-    // Print the list
-    list.print();
+	tree a;
+	a.avlCSVparser("ecom.csv");
 }
-
