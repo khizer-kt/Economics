d530e4da12530c75e800bd203549b40623c90464
Khizer Tariq <99535489+khizer-kt@users.noreply.github.com>
2022-12-31 19:16:03 +0500

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,56 +1,82 @@
 #pragma once
+
 //KT
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 using namespace std;
-int const capacity = 600000;
-int arr[capacity];
-
-
-
-class Hash{
+int const capacity2 = 600000;
+class Hash__ {
 public:
 	int hash_(string filename);
-	string get_(string filename);
 	bool isnum(string s);
-	void insert(int arr[], string filename);
+	void insert(string filename);
+	void test();
+	bool isnum2(std::string s);
+	string function(string filename);
 };
-void Hash::insert(int arr[], string filename) {
+void Hash__::insert(string filename) {
+	//cout << "CALLED";
 	int loc;
 	loc = hash_(filename);
-	string element = get_(filename);
+	int* arr;
+	arr = new int;
+	string element = function(filename);
 	arr[loc] = stoi(element);
+	cout << stoi(element);
+	int size = sizeof(arr) / sizeof(arr[0]);
+	//cout << "Call complete";
 }
-bool Hash::isnum(std::string s) { //num checker
+bool Hash__::isnum(std::string s) { //num checker
 	//returns true when num
+	//cout << "called numcheck";
 	return std::all_of(s.begin(), s.end(), ::isdigit);
 }
-string Hash::get_(string filename) {
+bool Hash__::isnum2(std::string s) {
+	for (char c : s) {
+		if (!isdigit(c)) {
+			return false;
+		}
+	}
+	return true;
+}
+int Hash__::hash_(string filename) { //get the locations for storage
+	string x = function(filename);
+	//cout << "calledhash";
+	int y = stoi(x);
+	return y % capacity2;
+}
+void Hash__::test() {
+	cout << "TEST FUNCTION" << endl;
+}
+string Hash__::function(string filename) {
 	ifstream file(filename);
-	string line, column;
+	string line, firstColumn;
+	//cout << "call";
+	int temp = 590000;
 	char a = ',';
-	int column_temp = 590000;
+	//cout << "outside loop";
 	while (getline(file, line)) {
 		// create a stringstream from the line
 		stringstream ss(line);
 
 		// get the first value before the comma and store it in firstColumn
-		getline(ss, column, a);
-
+		getline(ss, firstColumn, a);
+		//cout << "outside if";
 		// insert the firstColumn value into the tree
-		if (!isnum(column)) {
-			column = to_string(column_temp);
+		if (!isnum2(firstColumn)) {
+			firstColumn = to_string(temp);
+			//cout << firstColumn;
+			return firstColumn;
 		}
-		column_temp++;
-		
-		return column;
+		else {
+			//cout << "else";
+			//cout << firstColumn;
+			return firstColumn;
+		}
+		temp++;
 
 	}
-}
-int Hash::hash_(string filename) { //get the locations for storage
-	string x = get_(filename);
-	int y = stoi(x);
-	return y % capacity;
 }
