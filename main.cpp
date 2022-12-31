8a44358acd8c70bf9ee6fc218b72a958591ce454
Khizer Tariq <99535489+khizer-kt@users.noreply.github.com>
2023-01-01 01:33:47 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
-#include <iostream>
-#include "AVL.h"
-using namespace std;
+//#include <iostream>
+//#include "AVL.h"
+//#include "hash.h"
+//#include "AVL.h"
+//#include "linkedlist.h"
+//using namespace std;
+//int main() {
+	//tree a;
+	//a.avlCSVparser("ecom.csv");
+	//node* root = a.getRoot();
+	//a.preorder(root);
+	//foo("test.csv");
+	//tree b;
+	//b.avlCSVparser_2("ecom.csv");
+	//node* root = b.getRoot();
+	//b.preorder(root);
+	//int* arr = new int;
+	//Hash__ obj;
+	//obj.insert("ecom.csv", arr);
+	//LinkedList a;
+	//a.CSVparser("ecom.csv");
+	//a.print();
+//}
+
+#include "hash.h"
+
+
+
 int main() {
-	tree a;
-	a.avlCSVparser("test.csv");
-	//a.insert("Value i inserted");
-	node* root = a.getRoot();
-	a.preorder(root);
+    int* values = converter();
+
+    // Print the integer values
+    for (int i = 0; i < MAX_VALUES; i++) {
+        cout << values[i] << endl;
+    }
+
+    return 0;
 }
+
