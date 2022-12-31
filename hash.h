5f5a7da33488cf0ac719e783d58dc9a8cdf7e054
Khizer Tariq <99535489+khizer-kt@users.noreply.github.com>
2023-01-01 01:34:15 +0500

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,81 +1,56 @@
-#pragma once
-
-//KT
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <algorithm>
+#include <sstream>
+
 using namespace std;
-int const capacity2 = 600000;
-class Hash__ {
-public:
-	int hash_(string filename);
-	bool isnum(string s);
-	void insert(string filename);
-	void test();
-	bool isnum2(std::string s);
-	string function(string filename);
-};
-void Hash__::insert(string filename) {
-	//cout << "CALLED";
-	int loc;
-	loc = hash_(filename);
-	int* arr;
-	arr = new int;
-	string element = function(filename);
-	arr[loc] = stoi(element);
-	cout << stoi(element);
-	//cout << "Call complete";
-}
-bool Hash__::isnum(std::string s) { //num checker
-	//returns true when num
-	//cout << "called numcheck";
-	return std::all_of(s.begin(), s.end(), ::isdigit);
-}
-bool Hash__::isnum2(std::string s) {
-	for (char c : s) {
-		if (!isdigit(c)) {
-			return false;
-		}
-	}
-	return true;
-}
-int Hash__::hash_(string filename) { //get the locations for storage
-	string x = function(filename);
-	//cout << "calledhash";
-	int y = stoi(x);
-	return y % capacity2;
-}
-void Hash__::test() {
-	cout << "TEST FUNCTION" << endl;
-}
-string Hash__::function(string filename) {
-	ifstream file(filename);
-	string line, firstColumn;
-	//cout << "call";
-	int temp = 590000;
-	char a = ',';
-	//cout << "outside loop";
-	while (getline(file, line)) {
-		// create a stringstream from the line
-		stringstream ss(line);
 
-		// get the first value before the comma and store it in firstColumn
-		getline(ss, firstColumn, a);
-		//cout << "outside if";
-		// insert the firstColumn value into the tree
-		if (!isnum2(firstColumn)) {
-			firstColumn = to_string(temp);
-			//cout << firstColumn;
-			return firstColumn;
-		}
-		else {
-			//cout << "else";
-			//cout << firstColumn;
-			return firstColumn;
-		}
-		temp++;
+const int MAX_VALUES = 584530;  // Maximum number of values to store
 
-	}
+int* converter() {
+    static int values[MAX_VALUES];  // Array to store the values
+
+    // Open the CSV file
+    ifstream file("ecom.csv");
+
+    // Read the values from the first column
+    string line;
+    int nonIntCounter = 584526;
+    int i = 0;  // Index into the array
+    bool firstLine = true;  // Flag to indicate the first line
+    while (getline(file, line)) {
+        if (firstLine) {
+            // Skip the first line
+            firstLine = false;
+            continue;
+        }
+
+        // Split the line into fields
+        stringstream ss(line);
+        string value;
+        getline(ss, value, ',');
+
+        // Convert the value to an integer
+        int val;
+        if (!(stringstream(value) >> val)) {
+            // Assign a default value if the value is not an integer
+            val = nonIntCounter++;
+        }
+
+        values[i++] = val;
+    }
+
+    return values;
 }
+
+
+//int main() {
+//    int* values = converter();
+//
+//    // Print the integer values
+//    for (int i = 0; i < MAX_VALUES; i++) {
+//        cout << values[i] << endl;
+//    }
+//
+//    return 0;
+//}
